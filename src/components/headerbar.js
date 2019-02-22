2ac0ddac1469b21771f775132500d052fdf47774
Stoic Dev <stoicwebdev@gmail.com>
2019-02-22 11:28:42 -0500

    readme

--- src/components/headerbar.js
+++ src/components/headerbar.js
@@ -13,8 +13,9 @@ export default class HeaderBar extends Component {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end col">
           <Nav>
-            <Nav.Link className="custom-link" href="https://seancodydev.github.io/project-portfolio/">My Portfolio</Nav.Link>
-            <Nav.Link className="custom-link" href="https://www.openbrewerydb.org/">Open Brewery API</Nav.Link>
+            <Nav.Link className="custom-link" href="https://github.com/SeanCodyDev/open-brewery" target="_blank">View Repo</Nav.Link>
+            <Nav.Link className="custom-link" href="https://seancodydev.github.io/project-portfolio/" target="_blank">My Portfolio</Nav.Link>
+            <Nav.Link className="custom-link" href="https://www.openbrewerydb.org/" target="_blank">Open Brewery API</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
